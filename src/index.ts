19d412faaefef065b4113bad3aead5a8b06c1d68
Jean Paul Mayorga <jpmayorga@outlook.com>
2023-04-14 01:25:53 -0500

    fix: updating repo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,79 @@
-const a: number = 234;
+import express, { Request, Response } from "express";
+import { Client, LocalAuth } from "whatsapp-web.js";
+import QRCode from "qrcode-terminal";
+import dotenv from "dotenv";
 
-console.log({ a });
+import { logger } from "./utils";
+
+dotenv.config();
+
+async function server(whatsapp: Client) {
+  logger.info("server: initializing");
+  const server = express();
+  server.use(express.json());
+
+  server.post("/message", async (req: Request, res: Response) => {
+    interface Body {
+      message?: string;
+      to?: string;
+    }
+    const body = req.body as Body;
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ erorr: "Not authorized." });
+    }
+    if (token !== process.env.TOKEN_SECRET) {
+      return res.status(401).json({ erorr: "Not authorized." });
+    }
+    if (!body.message || !body.to) {
+      return res.status(400).json({ erorr: "Missing fields (message or to)" });
+    }
+    if (!body.to.startsWith("593")) {
+      return res
+        .status(400)
+        .json({ erorr: "Bad phone number, must start with 593" });
+    }
+
+    try {
+      const to = `${body.to}@c.us`;
+      const message = body.message;
+
+      logger.info({ to, message }, "whatsapp: start sending message");
+      const newMessage = await whatsapp.sendMessage(to, message);
+      logger.info(`whatsapp: message sent ${newMessage.id.id}`);
+
+      return res.status(200).json({ data: newMessage });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  });
+  server.listen(3000, () => {
+    logger.info("server: running on http://localhost:3000/");
+  });
+}
+
+async function main() {
+  logger.info("whatsapp: initializing");
+  const whatsapp = new Client({
+    authStrategy: new LocalAuth(),
+  });
+
+  whatsapp.initialize();
+
+  whatsapp.on("qr", (qr) => {
+    logger.info("whatsapp: qr code is ready!!");
+    QRCode.generate(qr, { small: true });
+  });
+  whatsapp.on("authenticated", () => {
+    logger.info("whatsapp: authenticated");
+  });
+  whatsapp.on("ready", () => {
+    logger.info("whatsapp: ready");
+    server(whatsapp);
+  });
+  whatsapp.on("auth_failure", (e) => {
+    logger.error({ message: e }, "Error: Authentication failed");
+  });
+}
+
+main();
