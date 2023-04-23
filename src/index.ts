7ab3833a8e12a79c79bc7be1fae61105f6ae643b
Jean Paul Mayorga <jpmayorga@outlook.com>
2023-04-23 01:00:54 -0500

    fix: adding listener

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import QRCode from "qrcode-terminal";
 
 import { logger } from "./utils";
 import { server } from "./server";
+import axios from "axios";
 
 async function main() {
   logger.info("whatsapp: initializing");
@@ -23,6 +24,20 @@ async function main() {
   whatsapp.on("loading_screen", () => logger.info("whatsapp: loading"));
   whatsapp.on("auth_failure", (e) => logger.error({ e }, "whatsapp: error"));
   whatsapp.on("ready", () => logger.info("whatsapp: ready"));
+
+  whatsapp.on("message", async (message) => {
+    if (message.type === "buttons_response") {
+      try {
+        await axios.put("https://estacaambato.vercel.app/api/whatsapp", {
+          method: "PUT",
+          body: { selectedButtonId: message.selectedButtonId },
+        });
+        logger.info(`severAPI: PUT sended ${message.selectedButtonId}`);
+      } catch {
+        logger.error("serverAPI: cannot save");
+      }
+    }
+  });
 
   server(whatsapp);
 }
